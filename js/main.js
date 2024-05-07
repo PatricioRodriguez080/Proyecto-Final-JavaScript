525dcb114e2de24a632f39602b50a0f8033c58ae
Patricio Rodriguez <patriciorodriguez080@gmail.com>
2024-05-07 19:42:45 -0300

    Cambio de colores

--- js/main.js
+++ js/main.js
@@ -225,10 +225,9 @@ function renderizarCarrito(arrayCarrito) {
         botonFinalizarCompra.addEventListener("click", (e) => {
             Swal.fire({
                 title: "Quiere finalizar la compra?",
-                text: "You won't be able to revert this!",
                 icon: "warning",
                 showCancelButton: true,
-                confirmButtonColor: "#3085d6",
+                confirmButtonColor: "#2f4f4f",
                 cancelButtonColor: "#d33",
                 confirmButtonText: "Confirmar!",
                 cancelButtonText: "Cancelar"
